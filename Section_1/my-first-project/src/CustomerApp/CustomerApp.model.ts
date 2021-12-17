79eb77b6055318c91d7eb27a26d3dba339031524
jeraldpraveen <jerald.vit@gmail.com>
2021-12-18 00:27:37 +0530

    17.Validation Object Model

--- Section_1/my-first-project/src/CustomerApp/CustomerApp.model.ts
+++ Section_1/my-first-project/src/CustomerApp/CustomerApp.model.ts
@@ -1,5 +1,24 @@
+import {
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
 export class Customer {
   CustomerCode: string = '';
   CustomerName: string = '';
   CustomerAmount: number = 0;
+
+  formCustomerGroup: FormGroup; // Create obj of FormGroup
+  constructor() {
+    var _builder = new FormBuilder();
+    this.formCustomerGroup = _builder.group({});
+  }
 }
+
+// Validation has 3 steps
+// create : We create the validaton Object Model
+// Connect : We connect the validation to UI
+// Check : IsItValid,IsItDirty ???
