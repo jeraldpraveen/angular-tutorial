06ae2df6103b34dd60ab80c10c42db65aa74a977
jeraldpraveen <jerald.vit@gmail.com>
2021-12-18 00:30:34 +0530

    18.MODAL-->Best place to add validaton

--- Section_1/my-first-project/src/CustomerApp/CustomerApp.model.ts
+++ Section_1/my-first-project/src/CustomerApp/CustomerApp.model.ts
@@ -6,6 +6,8 @@ import {
   FormBuilder,
 } from '@angular/forms';
 
+// Best place to add validaton is Model....
+// Formgroup --> FormBuilder --> FormControl --> Validators
 export class Customer {
   CustomerCode: string = '';
   CustomerName: string = '';
@@ -15,6 +17,20 @@ export class Customer {
   constructor() {
     var _builder = new FormBuilder();
     this.formCustomerGroup = _builder.group({});
+    //Inputs In Screen(Controls) --> Write Validations
+    //Name Input --> One Validation(required)
+    this.formCustomerGroup.addControl(
+      'CustomerNameControl',
+      new FormControl('', Validators.required)
+    );
+    // Code Input ---> Two Validations(required,4 letter numerics only)
+    var validationCollection = [];
+    validationCollection.push(Validators.required);
+    validationCollection.push(Validators.pattern('^[0-9]{4,4}$'));
+    this.formCustomerGroup.addControl(
+      'CustomerCodeControl',
+      new FormControl('', Validators.compose(validationCollection))
+    );
   }
 }
 
