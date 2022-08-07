38fa414724a53a4e31764c08101dbe53752c0d7c
jeraldpraveen <jerald.vit@gmail.com>
2022-08-07 15:05:13 +0530

    43.Angular Flow

--- 04-first-angular/src/app/jerald-app.ts
+++ 04-first-angular/src/app/jerald-app.ts
@@ -1,1 +1,26 @@
-console.log('Jerald Angular');
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello World</h1>
+    <p>Message From Jerald</p>
+    <app-welcome></app-welcome>`,
+})
+class HelloWorldComponent {}
+
+@Component({
+  selector: 'app-welcome',
+  template: `<h1>Welcome Component</h1>`,
+})
+class WelcomeComponent {}
+
+@NgModule({
+  bootstrap: [HelloWorldComponent],
+  declarations: [HelloWorldComponent, WelcomeComponent],
+  imports: [BrowserModule],
+})
+class JeraldModule {}
+
+platformBrowserDynamic().bootstrapModule(JeraldModule);
