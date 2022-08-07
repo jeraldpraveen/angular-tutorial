a1724574a7eed789657a241fe8f67ddbd12a48bc
jeraldpraveen <jerald.vit@gmail.com>
2022-08-08 02:35:49 +0530

    56. Custom Derivative with input

--- 06-directives/src/app/directives/box.directive.ts
+++ 06-directives/src/app/directives/box.directive.ts
@@ -1,12 +1,19 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBox]',
 })
 export class BoxDirective implements AfterViewInit {
+  @Input()
+  size = '1px';
+  type = 'solid';
+
+  @Input()
+  color = 'red';
+
   constructor(private element: ElementRef) {}
 
   ngAfterViewInit(): void {
-    this.element.nativeElement.style.border = '1px solid black';
+    this.element.nativeElement.style.border = `${this.size} ${this.type} ${this.color}`;
   }
 }
